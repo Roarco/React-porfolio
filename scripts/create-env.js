e2db378f3e45b26e470591c4743487d502866b65
Roarco <sebuscaroberth@gmail.com>
2021-09-23 20:37:30 -0500

    solucionando problema-variable de entorno

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,4 +1,4 @@
 
 const fs = require('fs');
 
-fs.writeFileSync('./.env', `API=${process.env.REACT_APP_API_URL}\n`);
+fs.writeFileSync('./.env', `REACT_APP_API_URL=${process.env.REACT_APP_API_URL}\n`);
